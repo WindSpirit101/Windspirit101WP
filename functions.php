65bfbc6aeb747567b4196f1c7a6b1d6d08ea283b
WindSpirit101 <100785677+WindSpirit101@users.noreply.github.com>
2022-05-10 11:19:11 -0500

    kk

--- functions.php
+++ functions.php
@@ -8,9 +8,10 @@
 ///}
 
 function enqueue_my_styles_and_scripts(){
-
+///Enqueueing our custom portfolio styles
   wp_enqueue_style('my-styles', get_stylesheet_directory_uri() .
   '/wp-theme/index.css', array(), '1.0.0', 'all');
+
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_my_styles_and_scripts');
